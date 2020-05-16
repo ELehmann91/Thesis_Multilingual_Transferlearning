58341793406ba9d08a7d839df5b8c9a94f0e545b
ELehmann91 <erik.lehmann91@gmail.com>
2020-05-16 14:54:58 +0200

    Update coicop_model.py

--- coicop_model.py
+++ coicop_model.py
@@ -201,6 +201,9 @@ class predictor:
         if text is None:
             n = random.randint(0, len(self.df))
             text = self.df['text'].iloc[n]
+            print('prediction',self.df['pred_cc5'].iloc[n])
+            if label_col is not None:
+                print('label',self.df[label_col].iloc[n])
         predict_func = self.get_predict_function()
         sampler = MaskingTextSampler(replacement="UNK", max_replace=0.7, token_pattern=None, bow=False)
         te = TextExplainer(sampler=sampler, position_dependent=True, random_state=42)
